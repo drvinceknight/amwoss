3ca1836cfe3aa180917449d485e922cee65afe98
Vince Knight <vincent.knight@gmail.com>
2019-11-16 15:07:54 +0000

    Tests pass for python.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import re
 import dwys
 
 pyin_pattern = re.compile(r"\\begin\{pyin\}\n(.*?)\\end\{pyin\}", re.DOTALL)
-pyout_pattern = re.compile(r"\\begin\{pyout\}\n(.*?)\\end\{pyout\}", re.DOTALL)
+pyout_pattern = re.compile(r"\\begin\{pyout\}\n(.*?)\n\\end\{pyout\}", re.DOTALL)
 Rin_pattern = re.compile(r"\\begin\{Rin\}\n(.*?)\\end\{Rin\}", re.DOTALL)
 Rout_pattern = re.compile(r"\\begin\{Rout\}\n(.*?)\\end\{Rout\}", re.DOTALL)
 pyexecution_command = "python"
@@ -30,16 +30,19 @@ for p in book:
 
             # Obtain a diff with the `Rscript` command
             try:
-                diff = list(dwys.diff(
+                diff, output, expected_output = dwys.diff(
                     input_code=input_code,
                     expected_output_code=output_code,
                     execution_command=execution_command,
-                ))
+                )
+                diff = list(diff)
                 try:
                     assert diff == []
                     print(f"{execution_command}: ✅")
                 except AssertionError:
                     print(f"{execution_command}: Input does not match output in {p}")
+                    print(f"Obtained output:\n{output}")
+                    print(f"Expected output:\n{expected_output}")
                     print(f"\t {diff}")
             except AssertionError:
                 print(f"{execution_command} Syntax error in {p}")
