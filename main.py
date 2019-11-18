085fa7fcba4491659f57e654c84f28d2aeb9adc5
Vince Knight <vincent.knight@gmail.com>
2019-11-18 09:14:50 +0000

    Add black and lintr checks for style.
    Note I'm pretty sure the lintr is not working correctly.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Run doctests
 """
 import pathlib
 import re
+import subprocess
+import tempfile
 
 import dwys
 
@@ -14,28 +16,37 @@ pyexecution_command = "python"
 Rexecution_command = "Rscript"
 
 book = pathlib.Path('./src/').glob('**/*tex')
+dir_for_python_input_files = tempfile.mkdtemp()
+dir_for_R_input_files = tempfile.mkdtemp()
 
-for p in book:
+for i, p in enumerate(book):
     if p.suffix == ".tex":
         print(f"Testing {p}")
         text = p.read_text()
 
-        for in_pattern, out_pattern, execution_command in (
-                (pyin_pattern, pyout_pattern, pyexecution_command),
-                (Rin_pattern, Rout_pattern, Rexecution_command)):
+        for in_pattern, out_pattern, execution_command, input_dir in (
+                (pyin_pattern, pyout_pattern, pyexecution_command, dir_for_python_input_files),
+                (Rin_pattern, Rout_pattern, Rexecution_command, dir_for_R_input_files),
+                ):
             # Parse the code
             input_code, output_code = dwys.parse(
                 string=text, in_pattern=in_pattern, out_pattern=out_pattern
             )
 
-            # Obtain a diff with the `Rscript` command
+            if execution_command == "python":
+                input_filename = f"{dir_for_python_input_files}/{i}.py"
+            else:
+                input_filename = f"{dir_for_python_input_files}/{i}.R"
+
             try:
                 diff, output, expected_output = dwys.diff(
                     input_code=input_code,
                     expected_output_code=output_code,
                     execution_command=execution_command,
+                    input_filename=input_filename,
                 )
                 diff = list(diff)
+
                 try:
                     assert diff == []
                     print(f"{execution_command}: ✅")
@@ -44,5 +55,11 @@ for p in book:
                     print(f"Obtained output:\n{output}")
                     print(f"Expected output:\n{expected_output}")
                     print(f"\t {diff}")
+
             except AssertionError:
                 print(f"{execution_command} Syntax error in {p}")
+
+print("Running black")
+subprocess.call(["black", "--check", "--diff", dir_for_python_input_files])
+print("Running lintr")
+subprocess.call(["Rscript", "-e",  "'lintr::lint(commandArgs(trailingOnly = TRUE))'" , dir_for_R_input_files])
