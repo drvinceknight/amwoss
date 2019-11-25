2d1d739d9fea13d5be9ffebe12698dc610e5ad64
Vince Knight <vincent.knight@gmail.com>
2019-11-25 10:08:39 +0000

    Add doctest framework (#21)
    * Start writing chapter 1.
    
    * Make modifications suggested by GP.
    
    Also included relevant TODO.
    
    * Move file from md to tex.
    
    * Write intro to R and Python chapters.
    
    * Add basic main file to compile full document.
    
    * Add book style files.
    
    * Write the basic code and python input envs.
    
    * Enhance code environments and use them.
    
    * Add env for explanation and add some options.
    
    * Make aesthetic changes (possibly improvements...)
    
    * Write a script too run dwys.
    
    WIP: all tests fail.
    
    * Tests pass for python.
    
    * Tests pass for R as well.
    
    * Make final steps to the doctesting:
    
    - Create environment
    - Add very basic documentation to README
    
    * Add black and lintr checks for style.
    
    Note I'm pretty sure the lintr is not working correctly.
    
    * look over language of chapter 2 (#23)
    
    * look over language of chapter 2
    
    * change minor wording after review with VK
    
    * Add better error reporting and change library
    
    * Create no-test environments
    
    Otherwise, the compiler would not work for some reason.
    
    * Write tasks.py
    
    This lets us control everything from a single command.
    
    Closes #22
    
    * Write travis file.
    
    * Configure shell conda to use conda activate
    
    * Use source activate instead of conda activate.
    
    * Add black to environment.
    
    * Add latexmk to travis build.
    
    * Add a texlive library.
    
    * Install texlive directly.
    
    * Ensure latexmk is installed.
    
    * Fix apt-get command.
    
    * Add missing space.
    
    * Install texlive extra.
    
    * Add xetex tex-live distribution.
    
    * Add texlive-lang-greek
    
    * Add a latex font library.
    
    * Add a python error.
    
    * Add exit codes so they are reported properly by travis.
    
    * Fix Python error but add R syntax error.
    
    * Fix R error but add latex error.
    
    * Compile with nonstop mode.
    
    * Fix latex error.
    
    * Fix wording of sty file documentation

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 Applied mathematics problems with Open Source Software: Operational Research with Python and R.
 
+## Create the Conda environment:
+
+Assuming you have anaconda on your machine, run:
+
+    $ conda env create -f environment.yml
+
+There are some further dependencies that are needed.
+
+Activate the environment:
+
+    $ conda activate amwpwoss
+    $ inv env
+
 ## Compilation instructions
 
 Run:
 
-    $ latexmk --xelatex -shell-escape main.tex
+    $ inv compile
+
+## Doctesting
+
+Run:
+
+    $ inv doctest
---
+++ tasks.py
@@ -0,0 +1,113 @@
+import pathlib
+import re
+import subprocess
+import tempfile
+import sys
+
+import dwys
+
+from invoke import task
+
+@task
+def env(c):
+    """
+    Finish the installation of any libraries and packages need for the
+    environment.
+    """
+    c.run("""Rscript -e 'install.packages("simmer", repos="http://cran.us.r-project.org")'""")
+
+@task
+def compile(c):
+    """
+    Compile the LaTeX document.
+    """
+    c.run("cd src; latexmk  -interaction=nonstopmode  --xelatex -shell-escape main.tex")
+
+
+@task
+def doctest(c):
+    """
+    Run doctests on all LaTeX documents
+
+    - Checks code gives expected output using dwys
+    - Check style of python code with black
+    - Check style of R code with lintr (TODO Check that this works)
+    """
+    pyin_pattern = re.compile(r"\\begin\{pyin\}\n(.*?)\\end\{pyin\}", re.DOTALL)
+    pyout_pattern = re.compile(r"\\begin\{pyout\}\n(.*?)\n\\end\{pyout\}", re.DOTALL)
+    Rin_pattern = re.compile(r"\\begin\{Rin\}\n(.*?)\\end\{Rin\}", re.DOTALL)
+    Rout_pattern = re.compile(r"\\begin\{Rout\}\n(.*?)\n\\end\{Rout\}", re.DOTALL)
+    pyexecution_command = "python"
+    Rexecution_command = "Rscript"
+
+    book = pathlib.Path("./src/").glob("**/*tex")
+    dir_for_python_input_files = tempfile.mkdtemp()
+    dir_for_R_input_files = tempfile.mkdtemp()
+
+    exit_codes = []
+    for i, p in enumerate(book):
+        if p.suffix == ".tex":
+            print(f"Testing {p}")
+            text = p.read_text()
+
+            for in_pattern, out_pattern, execution_command, input_dir in (
+                (
+                    pyin_pattern,
+                    pyout_pattern,
+                    pyexecution_command,
+                    dir_for_python_input_files,
+                ),
+                (Rin_pattern, Rout_pattern, Rexecution_command, dir_for_R_input_files),
+            ):
+                # Parse the code
+                input_code, output_code = dwys.parse(
+                    string=text, in_pattern=in_pattern, out_pattern=out_pattern
+                )
+
+                if execution_command == "python":
+                    input_filename = f"{dir_for_python_input_files}/{i}.py"
+                else:
+                    input_filename = f"{dir_for_R_input_files}/{i}.R"
+
+                try:
+                    diff, output, expected_output = dwys.diff(
+                        input_code=input_code,
+                        expected_output_code=output_code,
+                        execution_command=execution_command,
+                        input_filename=input_filename,
+                    )
+                    diff = list(diff)
+
+                    try:
+                        assert diff == []
+                        exit_codes.append(0)
+                        print(f"{execution_command}: ✅")
+                    except AssertionError:
+                        print(
+                            f"{execution_command}: ❌ Input does not match output in {p}"
+                        )
+                        print(f"Obtained output:\n{output}")
+                        print(f"Expected output:\n{expected_output}")
+                        print(f"\t {diff}")
+                        exit_codes.append(1)
+
+                except AssertionError:
+                    print(f"{execution_command}: ❌ Syntax error in {p}")
+                    print(input_filename)
+                    exit_codes.append(1)
+
+    print("Running black")
+    ec =  subprocess.call(["black", "--check", "--diff", dir_for_python_input_files])
+    exit_codes.append(ec)
+
+    print("Running lintr")
+    ec = subprocess.call(
+        [
+            "Rscript",
+            "-e",
+            "'lintr::lint(commandArgs(trailingOnly = TRUE))'",
+            dir_for_R_input_files,
+        ]
+    )
+    exit_codes.append(ec)
+    sys.exit(max(exit_codes))
