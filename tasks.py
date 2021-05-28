22a71d5a4f02f5b8534a744cc1c969ea509b55dd
Vince Knight <vincent.knight@gmail.com>
2021-05-28 13:49:28 +0100

    Fix more stuff.

--- tasks.py
+++ tasks.py
@@ -28,9 +28,9 @@ def env(c):
     anaconda environment file `environment.yml` (for example some R packages do
     not have anaconda binaries).
     """
-    c.run(
-        """Rscript -e 'install.packages("simmer", repos="http://cran.us.r-project.org")'"""
-    )
+#    c.run(
+#        """Rscript -e 'install.packages("simmer", repos="http://cran.us.r-project.org")'"""
+#    )
     c.run(
         """Rscript -e 'install.packages("expm", repos="http://cran.us.r-project.org")'"""
     )
@@ -43,7 +43,7 @@ def env(c):
     else:
         c.run("sudo apt-get install coinor-libcbc-dev coinor-libclp-dev")
     c.run(
-        """Rscript -e 'install.packages("ROI")'"""
+        """Rscript -e 'install.packages("ROI", repos="http://cran.us.r-project.org")'"""
     )
     c.run(
         """Rscript -e 'remotes::install_github("dirkschumacher/rcbc")'"""
