f575f46919888ab2e710acf471f0922a73e67028
Geraint Palmer <palmer.geraint@googlemail.com>
2021-10-11 15:24:19 +0100

    make tsp plots prettier

--- src/assets/final-tsp-tours-with-python/main.py
+++ src/assets/final-tsp-tours-with-python/main.py
@@ -7,11 +7,13 @@ import sympy as sym
 def plot_tour(ax, x, y, tour):
         xs = x[tour]
         ys = y[tour]
-        ax.quiver(xs[:-1], ys[:-1], xs[1:]-xs[:-1], ys[1:]-ys[:-1], scale_units='xy', angles='xy', scale=2)
-        ax.plot(xs, ys)
+        ax.quiver(xs[:-1], ys[:-1], xs[1:]-xs[:-1], ys[1:]-ys[:-1], scale_units='xy', angles='xy', scale=1.8, width=0.0001, headwidth=180, headlength=180, headaxislength=140)
+        ax.plot(xs, ys, c='black', linewidth=1)
+        ax.scatter(xs, ys, s=170, zorder=3, c='white', edgecolor='black')
+        ax.tick_params(labelsize=6)
 
         for i, (x, y) in enumerate(zip(xs[:-1], ys[:-1])):
-            ax.annotate(str(i), (x, y), backgroundcolor="grey", fontsize='xx-small')
+            ax.annotate(str(i), (x, y), fontsize='small', ha='center', va='center')
 
 seed = 1
 number_of_stops = 13
@@ -24,7 +26,7 @@ swap_stops_solution = [0, 7, 2, 8, 5, 3, 1, 9, 12, 11, 4, 10, 6, 0]
 swap_paths_solution = [0, 8, 5, 3, 1, 9, 12, 11, 4, 10, 6, 2, 7, 0]
 
 # Draw plot
-fig, axarr = plt.subplots(1, 3, figsize=(15, 4))
+fig, axarr = plt.subplots(1, 3, figsize=(13, 4))
 plot_tour(ax=axarr[0], x=xs, y=ys, tour=initial_candidate)
 axarr[0].set_title("Initial candidate")
 
